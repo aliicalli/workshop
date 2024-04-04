c7a9c414e29a11b553690c9fede9be2824ff79af
Ali Çallı <alicalli0210@gmail.com>
2024-04-04 22:26:52 +0300

    "Added Resilience4j for fault tolerance and improved logging in microservices"
    Here's a breakdown of the changes:
    
    In the pom.xml file, dependencies for spring-cloud-starter-circuitbreaker-resilience4j and spring-boot-starter-actuator were added. This indicates that the Resilience4j library is being used for fault tolerance in the microservices, and the Actuator is being used for monitoring and managing the application.
    In the OrderController.java file, the placeOrder method has been modified to use Resilience4j's CircuitBreaker, TimeLimiter, and Retry annotations. This means that the method now has circuit breaking, timeout, and retry capabilities. A fallback method has also been added to handle cases when the circuit is open or a call to the method fails.
    In the application.properties file, several properties related to Resilience4j have been added. These properties configure the behavior of the circuit breaker, timeout, and retry features.
    In the InventoryService.java file, a delay has been added to the isInStock method, and logging statements have been added before and after the delay. This could be for testing the timeout and retry features.
    In the OrderService.java file, the placeOrder method has been modified to return a string message. This could be the message that is returned to the client when an order is successfully placed.

--- microservices/inventory-service/src/main/java/com/alicalli/inventoryservice/service/InventoryService.java
+++ microservices/inventory-service/src/main/java/com/alicalli/inventoryservice/service/InventoryService.java
@@ -4,17 +4,25 @@ import com.alicalli.inventoryservice.dto.InventoryResponse;
 import com.alicalli.inventoryservice.repository.InventoryRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
   private final InventoryRepository inventoryRepository;
 
   @Transactional(readOnly = true)
+  @SneakyThrows
   public List<InventoryResponse> isInStock(List<String> skuCode) {
+    log.info("Wait Started");
+    Thread.sleep(10000);
+    log.info("Wait Ended");
     return inventoryRepository
       .findBySkuCodeIn(skuCode)
       .stream()
--- microservices/order-service/src/main/java/com/alicalli/orderservice/controller/OrderController.java
+++ microservices/order-service/src/main/java/com/alicalli/orderservice/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.alicalli.orderservice.controller;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alicalli.orderservice.dto.OrderRequest;
 import com.alicalli.orderservice.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,10 +24,25 @@ public class OrderController {
 
   private final OrderService orderService;
 
-  @PostMapping()
+  @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public String plaveOrder(@RequestBody OrderRequest orderRequest) {
-    orderService.placeOrder(orderRequest);
-    return "Order Placed Successfully";
+  @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+  @TimeLimiter(name = "inventory")
+  @Retry(name = "inventory")
+  public CompletableFuture<String> placeOrder(
+    @RequestBody OrderRequest orderRequest
+  ) {
+    return CompletableFuture.supplyAsync(() ->
+      orderService.placeOrder(orderRequest)
+    );
+  }
+
+  public CompletableFuture<String> fallbackMethod(
+    OrderRequest orderRequest,
+    RuntimeException runtimeException
+  ) {
+    return CompletableFuture.supplyAsync(() ->
+      "Oops! Something went wrong, please order after some time!"
+    );
   }
 }
